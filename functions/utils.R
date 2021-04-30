95a04b1c2aaeb6626a9cb86088efb2e8688d1ab5
Adam Howes <adamhowes@outlook.com>
2021-04-30 17:27:59 +0100

    Safer to check that the columns are present

--- functions/utils.R
+++ functions/utils.R
@@ -7,28 +7,34 @@ safe_saveRDS <- function(object, output_dir, file) {
 }
 
 rename_df <- function(df) {
-  df$geometry <- recode_factor(df$geometry,            
-    "CI2012DHS" = "Cote d'Ivoire 2012 DHS", 
-    "MW2015DHS" = "Malawi 2015 DHS",
-    "TZ2012AIS" = "Tanzania 2012 AIS",
-    "ZW2015DHS" = "Zimbabwe 2015 DHS"
-  )
+  if("geometry" %in% names(df)) {
+    df$geometry <- recode_factor(df$geometry,            
+      "CI2012DHS" = "Cote d'Ivoire 2012 DHS", 
+      "MW2015DHS" = "Malawi 2015 DHS",
+      "TZ2012AIS" = "Tanzania 2012 AIS",
+      "ZW2015DHS" = "Zimbabwe 2015 DHS"
+    )
+  }
   
-  df$inf_model <- recode_factor(df$inf_model,
-    "constant" = "Constant",
-    "iid" = "IID", 
-    "icar" = "Besag", 
-    "bym" = "BYM2", 
-    "fck" = "FCK", 
-    "ck" = "CK", 
-    "fik" = "FIK",
-    "ik" = "IK"
-  )
+  if("inf_model" %in% names(df)) {
+    df$inf_model <- recode_factor(df$inf_model,
+      "constant" = "Constant",
+      "iid" = "IID", 
+      "icar" = "Besag", 
+      "bym" = "BYM2", 
+      "fck" = "FCK", 
+      "ck" = "CK", 
+      "fik" = "FIK",
+      "ik" = "IK"
+    )
+  }
   
-  df$type <- recode_factor(df$type, 
-    "loo" = "LOO", 
-    "sloo" = "SLOO"
-  )
+  if("type" %in% names(df)) {
+    df$type <- recode_factor(df$type, 
+      "loo" = "LOO", 
+      "sloo" = "SLOO"
+    )
+  }
   
   return(df)
 }
